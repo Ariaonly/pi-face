4e5d0947ecb37547e29cf6819f888c664038dfec
Ariaonly <2632898965@qq.com>
2025-11-13 18:54:48 +0800

    修复上一步的错误

--- docker.sh
+++ docker.sh
@@ -4,6 +4,8 @@ docker run -it --name face \
   --mount type=bind,source="$HOME/pro/face",target=/data \
   --network host \
   face_recog:pi5 \
-  bash
+  /bin/bash
 
 # 如何查看摄像头上的按键的事件设备号
+ls /dev/input/event*
+# 然后你可以拔下来摄像头再查看一遍对比不同
